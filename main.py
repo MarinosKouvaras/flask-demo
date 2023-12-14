3de67368ae20fcdf5aeaabed59954a74eb7e71d3
MarinosKouvaras <ap23011@hua.gr>
2023-12-14 21:00:21 +0200

    Simple form

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 from flask import Flask
+from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello!"
+    name='Marinos'
+    lastname='Kouvaras'
+    return render_template('index.html', myname=name, mylastname=lastname)
+
+@app.route('/name')
+def name():
+    return "Marinos Kouvaras"
+
+@app.route('/form', methods=['GET', 'POST'])
+def show_form():
+    if request.method == 'GET':
+        return render_template('form.html')
+    else:
+        name = request.form['name']
+        surname = request.form['surname']
+        return render_template('index.html', yourname= name, yoursurname=surname)
